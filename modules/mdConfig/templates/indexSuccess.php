6857812d274def0c623f3880a4c2d445661d6c28
Bruno <brunopastor92@gmail.com>
2013-02-06 18:59:26 -0200

    agregados i18n

--- modules/mdConfig/templates/indexSuccess.php
+++ modules/mdConfig/templates/indexSuccess.php
@@ -4,7 +4,7 @@
 <?php slot('nav') ?>Configuracion<?php end_slot(); ?>
 
 <div id="sf_admin_container">
-  <h1><?php echo __('Home_List', array(), 'messages') ?></h1>
+  <h1><?php echo __('Config_Titulo', array(), 'messages') ?></h1>
 
   <?php include_partial('mdConfig/flashes') ?>
 
@@ -17,7 +17,7 @@
   </div>
 
   <div id="sf_admin_content">
-    <form action="<?php echo url_for('md_log_collection', array('action' => 'batch')) ?>" method="post">
+    <form action="<?php echo url_for('md_config', array('action' => 'batch')) ?>" method="post">
     <?php include_partial('mdConfig/list', array('pager' => $pager, 'sort' => $sort, 'helper' => $helper)) ?>
     <ul class="sf_admin_actions">
       <?php include_partial('mdConfig/list_batch_actions', array('helper' => $helper)) ?>
